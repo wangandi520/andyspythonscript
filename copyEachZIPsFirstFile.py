955fdae857e9c2b42bd4df98af2b0f1679cd3b78
wangandi520 <wangandi520@gmail.com>
2021-05-15 12:20:34 +0800

    support subdir

--- copyEachZIPsFirstFile.py
+++ copyEachZIPsFirstFile.py
@@ -22,16 +22,22 @@ def main():
             zf.extract(tempFile01,'cover')
             os.system('rename cover\\' + tempFile01 + ' ' + newFileName)
         else:
-            print('Not support subdir.')
-            # tempFile02 = sorted(zf.namelist())[1]
-            # print(sorted(zf.namelist())[0].encode('cp437').decode('gbk'))
-            # print(sorted(zf.namelist())[1].encode('cp437').decode('gbk'))
-            # print(os.path.basename(tempFile02))
-            # newFileName = os.path.splitext(onezip)[0] + os.path.splitext(os.path.basename(tempFile02))[1]
-            # zf.extract(tempFile02,'cover')
-            # os.system('rename ' + tempFile02.encode('cp437').decode('gbk') + newFileName)
-            # print('rename "' + tempFile02.encode('cp437').decode('gbk') + '" ' + newFileName)
-    
-        
+            tempFile02 = sorted(zf.namelist())[1]
+            if tempFile02.endswith('.db'):
+                tempFile02 = sorted(zf.namelist())[2]
+            zf.extract(tempFile02,'cover')
+            tempFiles = os.listdir('cover')
+            tempAllDirs = []
+            for file in tempFiles:
+                if (os.path.isdir(os.path.join('cover', file))):
+                    tempAllDirs.append(file)
+            tempFileName = os.listdir('cover\\' + tempAllDirs[0])
+            newFileName = os.path.splitext(onezip)[0] + os.path.splitext(os.path.basename(tempFile02))[1]
+            tempCmd01 = 'rename "cover\\' + tempAllDirs[0] + '\\' + tempFileName[0] + '" ' + newFileName
+            os.system(tempCmd01)
+            tempCmd02 = 'move cover\\"' + tempAllDirs[0] + '\\' + newFileName + '" cover\\'
+            os.system(tempCmd02)
+            os.system('rmdir cover\\"' + tempAllDirs[0])
+
 if __name__ == '__main__':
     main()
