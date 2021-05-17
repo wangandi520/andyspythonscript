b66600c103d14f611c9919563370dcfa8a4a23ad
wangandi520 <wangandi520@gmail.com>
2021-05-17 09:56:32 +0800

    readme

--- README.md
+++ README.md
@@ -2,19 +2,19 @@
 
 ### 说明：
 
-copyEachFoldersFirstFile.py
+### copyEachFoldersFirstFile.py
 
 会把同级目录下的所有文件夹的第一个文件复制到cover文件夹内
 
 用途：复制出漫画封面
 
-copyEachZIPsFirstFile.py
+### copyEachZIPsFirstFile.py
 
 会把同级目录下的所有zip的第一个文件复制到cover文件夹内
 
 用途：复制出漫画封面
 
-renameToTC.py
+### renameToTC.py
 
 会把[换成[[]，]换成[]]
 例如
@@ -24,5 +24,20 @@ renameToTC.py
 
 用途：total commander 中ctrl+m批量重命名多卷漫画使用
 
+### allFolderFilesRename.py
+
+同级目录下有数个文件夹，每个文件夹里数个文件，没有二级文件夹。
+
+重命名的格式，会自动识别扩展名:
+
+第一个文件夹的文件夹01_001.jpg，到01_030.jpg，如果一共30个文件，扩展名是jpg的话
+
+第二个文件夹的文件夹02_001.jpg，到02_030.jpg，如果一共30个文件，扩展名是jpg的话
+
+以此类推
+
+如果把allFolderFilesRename.py改名其他名字，如123_.py，那文件名就是123_01_01.jpg
+
+第一个文件夹的数字，请修改dirCount
 
 ### 使用前请备份，防止文件名不符合你的需求
---
+++ allFolderFilesRename.py
@@ -0,0 +1,29 @@
+# encoding:utf-8
+
+import os.path
+import sys
+
+def main(fileName):
+    filePrefix = ''
+    if (os.path.basename(fileName) == 'allFolderFilesRename.py'):
+        filePrefix = ''
+    else:
+        filePrefix = os.path.basename(fileName)[0:-3]
+    allFiles = os.listdir('.')
+    allDirs = []
+    for file in allFiles:
+        if (os.path.isdir(file)):
+            allDirs.append(file)
+    
+    dirCount = 1
+    for dir in allDirs:
+        tempFilesName = os.listdir(dir)
+        fileCount = 1
+        for fileName in tempFilesName:
+            imputCmd = 'rename "' + dir + '\\' + fileName + '" ' + filePrefix + str(dirCount).zfill(2) + '_' + str(fileCount).zfill(3) + os.path.splitext(fileName)[1]
+            print(imputCmd)
+            os.system(imputCmd)
+            fileCount = fileCount + 1
+        dirCount = dirCount + 1
+if __name__ == '__main__':
+    main(sys.argv[0])
