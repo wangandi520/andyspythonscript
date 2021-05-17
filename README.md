1d1d22eeec1e749ee2fd1452b93391f7a60f1055
wangandi520 <wangandi520@gmail.com>
2021-05-17 09:51:58 +0800

    add renametc

--- README.md
+++ README.md
@@ -1,9 +1,28 @@
-## 复制每个文件夹，zip文件夹的第一个文件
+## python处理脚本几个
 
 ### 说明：
 
-会把zip的第一个文件复制到cover文件夹内
+copyEachFoldersFirstFile.py
+
+会把同级目录下的所有文件夹的第一个文件复制到cover文件夹内
 
 用途：复制出漫画封面
+
+copyEachZIPsFirstFile.py
+
+会把同级目录下的所有zip的第一个文件复制到cover文件夹内
+
+用途：复制出漫画封面
+
+renameToTC.py
+
+会把[换成[[]，]换成[]]
+例如
+[頭文字D_InitialD][重野秀一][尖端]
+变成
+[[]頭文字D_InitialD[]][[]重野秀一[]][[]尖端[]]Vol_[C]
+
+用途：total commander 中ctrl+m批量重命名多卷漫画使用
+
 
 ### 使用前请备份，防止文件名不符合你的需求
---
+++ renameToTC.py
@@ -0,0 +1,26 @@
+# encoding:utf-8
+
+import os
+
+def main():
+    allFiles = os.listdir('.')
+    allTXTs = []
+    for file in allFiles:
+        if (file != 'renameToTC.py'):
+            allTXTs.append(file)
+    
+    oldName = allTXTs[0]
+    newName = ''
+    for i in oldName:
+        if i == '[':
+            newName = newName + '[[]'
+        elif i == ']':
+            newName = newName + '[]]'
+        else:
+            newName = newName + i
+    newName = newName + 'Vol_[C]'
+    os.system('rename ' + oldName + ' ' + newName)
+    #print(newName)
+        
+if __name__ == '__main__':
+    main()
