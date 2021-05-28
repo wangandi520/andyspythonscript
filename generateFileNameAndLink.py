51308d9192579f96682c1f88d15d792b5695ebd6
wangandi520 <wangandi520@gmail.com>
2021-05-28 21:59:09 +0800

    improve css

--- generateFileNameAndLink.py
+++ generateFileNameAndLink.py
@@ -29,48 +29,6 @@ def formatFileSize(sizeBytes):
     return format(result,'.2f') + suffix
         
 def writefile(filereadlines):
-    newfile = open('index.html', mode='w', encoding='UTF-8')
-    newfile.writelines(filereadlines)
-    newfile.close()     
-    
-def main():
-    # 是否显示文件大小，show file size = 1, no file size = 0
-    showFileSize = 1
-    # 显示完整地址还是只显示文件名，show all address = 1, only show file name = 0
-    showAllAddress = 1
-    # 第一栏的宽度，first column width
-    columnWidth = 550
-    # 显示边框，show table border = 1, no border = 0# encoding:utf-8
-
-import os
-
-def formatFileSize(sizeBytes):
-    sizeBytes = float(sizeBytes)
-    result = float(abs(sizeBytes))
-    suffix = "B";
-    if(result>1024):
-        suffix = "KB"
-        mult = 1024
-        result = result / 1024
-    if(result > 1024):
-        suffix = "MB"
-        mult *= 1024
-        result = result / 1024
-    if (result > 1024) :
-        suffix = "GB"
-        mult *= 1024
-        result = result / 1024
-    if (result > 1024) :
-        suffix = "TB"
-        mult *= 1024
-        result = result / 1000
-    if (result > 1024) :
-        suffix = "PB"
-        mult *= 1024
-        result = result / 1024
-    return format(result,'.2f') + suffix
-        
-def writefile(filereadlines):
     fileName = os.getcwd().split('\\')[-1] + '.html'
     newfile = open(fileName, mode='w', encoding='UTF-8')
     newfile.writelines(filereadlines)
@@ -81,19 +39,22 @@ def main():
     showFileSize = 1
     # 显示完整地址还是只显示文件名，show all address = 1, only show file name = 0
     showAllAddress = 1
-    # 第一栏的宽度，first column width
-    columnWidth = 550
+    # 第一栏的宽度，first column width %
+    columnWidth = 80
     # 显示边框，show table border = 1, no border = 0
     showTableBorder = 1
     # 显示第一行，show first line = 1, no first line = 0(file name, sha1, file size)
     showFirstLine = 1
     # 是否显示处理过程, show process details = 1, no detils = 0
     showProcessDetails = 1
-    # 键盘按键抬起立刻搜索 = 'onkeyup'，还是按回车搜索 = 'onchange'， 文件数大于两万建议后者
+    # 键盘按键抬起立刻搜索 = 'onkeyup'，还是按回车搜索 = 'onchange'，文件数大于两万建议后者
     howToReactSearch = 'onkeyup'
     
     title = os.getcwd().split('\\')[-1]
-    outputFile = '<html><head><title>' + title + '</title><style>table,td{border:' + str(showTableBorder) +'px solid #000000;table-layout:fixed;border-collapse:collapse;}a{text-decoration: none;}td{width:100px;}table tr td:first-child{width:' + str(columnWidth) +'px}tr:hover{background-color:#eee;}</style><script type="text/javascript" language="JavaScript">function onSearch(){searchContent = document.getElementById(\'mySearch\').value;var storeId = document.getElementById(\'allFileTable\');var rowsLength = storeId.rows.length;for(var i=1;i<rowsLength;i++){var searchText = storeId.rows[i].cells[0].innerHTML;if(searchText.match(searchContent) || searchText.toUpperCase().match(searchContent.toUpperCase())){storeId.rows[i].style.display=\'\';}else{storeId.rows[i].style.display=\'none\';}}}</script></head><body><div>\n<table id="allFileTable">'
+    outputFile = '<html><head><title>' + title + '</title>\n'
+    outputFile = outputFile + '<style>body{width:90%;}table,td{border:' + str(showTableBorder) +'px solid #000000;table-layout:fixed;border-collapse:collapse;}a{text-decoration: none;}td{width:10%;}table tr td:first-child{width:' + str(columnWidth) +'%}tr:hover{background-color:#eee;}</style>\n'
+    outputFile = outputFile + '<script type="text/javascript" language="JavaScript">function onSearch(){searchContent = document.getElementById(\'mySearch\').value;var storeId = document.getElementById(\'allFileTable\');var rowsLength = storeId.rows.length;for(var i=1;i<rowsLength;i++){var searchText = storeId.rows[i].cells[0].innerHTML;if(searchText.match(searchContent) || searchText.toUpperCase().match(searchContent.toUpperCase())){storeId.rows[i].style.display=\'\';}else{storeId.rows[i].style.display=\'none\';}}}</script>\n'
+    outputFile = outputFile + '</head><body><div>\n<table id="allFileTable">'
     if showFirstLine:
         if showFileSize:
             outputFile = outputFile + '<tr><td><span id="fileNameID"></span><input type="text" id="mySearch" ' + howToReactSearch + '="onSearch()" placeholder="搜索..."></td><td>Size</td></tr>'
