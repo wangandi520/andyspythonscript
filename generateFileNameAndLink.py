c97399030bd2a4e77b29e7e9aa17585480f029d9
wangandi520 <wangandi520@gmail.com>
2021-05-28 21:05:38 +0800

    file. folder, size count

--- generateFileNameAndLink.py
+++ generateFileNameAndLink.py
@@ -50,11 +50,17 @@ def main():
     outputFile = '<html><head><title>index.html</title><style>table,td{border:' + str(showTableBorder) +'px solid #000000;table-layout:fixed;border-collapse:collapse;}a{text-decoration: none;}td{width:100px;}table tr td:first-child{width:' + str(columnWidth) +'px}tr:hover{background-color:#eee;}</style><script type="text/javascript" language="JavaScript">function onSearch(){searchContent = document.getElementById(\'mySearch\').value;var storeId = document.getElementById(\'allFileTable\');var rowsLength = storeId.rows.length;for(var i=1;i<rowsLength;i++){var searchText = storeId.rows[i].cells[0].innerHTML;if(searchText.match(searchContent) || searchText.toUpperCase().match(searchContent.toUpperCase())){storeId.rows[i].style.display=\'\';}else{storeId.rows[i].style.display=\'none\';}}}</script></head><body><div>\n<table id="allFileTable">'
     if showFirstLine:
         if showFileSize:
-            outputFile = outputFile + '<tr><td>File name:<input type="text" id="mySearch" onkeyup="onSearch()" placeholder="搜索..."></td><td>Size</td></tr>'
+            outputFile = outputFile + '<tr><td><span id="fileNameID"></span><input type="text" id="mySearch" onkeyup="onSearch()" placeholder="搜索..."></td><td>Size</td></tr>'
         else:
             outputFile = outputFile + '<tr><td>File name:<input type="text" id="mySearch" onkeyup="onSearch()" placeholder="搜索..."></td></tr>'
+    fileCount = 0
+    fileSizeCount = 0
+    folderCount = 0
     for root, dirs, files in os.walk(".", topdown=False):
+        for eachdir in dirs:
+            folderCount = folderCount + 1
         for name in files:
+            fileCount = fileCount + 1
             loc = os.path.join(root, name)[2:]
             if showProcessDetails:
                 print(loc)
@@ -62,14 +68,17 @@ def main():
                 showName = loc
             else:
                 showName = name
-            fileSize = formatFileSize(os.path.getsize(loc))
             if showFileSize:
+                fileSize = formatFileSize(os.path.getsize(loc))
+                fileSizeCount = fileSizeCount + os.path.getsize(loc)
                 outputFile = outputFile + '<tr><td><a href="' + loc + '">' + showName + '</td><td>' + fileSize + '</a></tr>\n'
             else:
                 outputFile = outputFile + '<tr><td><a href="' + loc + '">' + loc + '</a></td></tr>\n'
-            # print(os.path.join(root, name))
-            # print(os.path.join(root, name))
     outputFile = outputFile + '</td></table></div></body></html>'
+    outputFile = outputFile + '<script type="text/javascript" language="JavaScript">document.getElementById("fileNameID").innerHTML = "Name (' + str(fileCount) + ' files in ' + str(folderCount) + ' folders'
+    if showFileSize:
+        outputFile = outputFile + ', '+ formatFileSize(fileSizeCount)
+    outputFile = outputFile +  ') ";</script>'
     writefile(outputFile)
     
 if __name__ == '__main__':
