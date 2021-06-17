65d0b83f28ddcddb8e0d704bf9ffdc1bc69e35e5
wangandi520 <wangandi520@gmail.com>
2021-06-17 19:43:51 +0800

    support log and recover

--- renameAnyFolderNameToFile.py
+++ renameAnyFolderNameToFile.py
@@ -5,6 +5,11 @@ from pathlib import Path
 import sys
 import os
     
+def writefile(filereadlines):
+    newfile = open('renameAnyFolderNameToFile.txt', mode='w', encoding='UTF-8')
+    newfile.writelines(filereadlines)
+    newfile.close()
+    
 def main(inputPath):
     # get folder name
     if Path.is_dir(Path(inputPath)):
@@ -15,22 +20,38 @@ def main(inputPath):
         if folderName.endswith('完]') or folderName.endswith('未]'):
             loc = folderName.rfind('[')
             folderName = folderName[:loc]
+            
         # rename file
+        cmdLog = []
+        cmdLog.append('Log:\n')
+        recoverLog = []
+        recoverLog.append('Recover:\n')
         fileCount = 0
         lastFileName = ''
+        lastFileOldName = ''
         for file in Path(inputPath).glob('**/*'):
             newFileName = folderName + 'Vol_' + str(fileCount + 1).zfill(2) + file.suffix
             imputCmd = 'rename "' + str(Path(inputPath).joinpath(file)) + '" "' + newFileName + '"'
+            recoverCmd = 'rename "' + str(Path(inputPath).joinpath(newFileName)) + '" "' + file.name + '"'
+            cmdLog.append(imputCmd + '\n')
+            recoverLog.append(recoverCmd + '\n')
             print(file.name + '  ->  ' + newFileName)
             os.system(imputCmd)
             fileCount = fileCount + 1
             lastFileName = newFileName
-            
+            lastFileOldName = file.name
+        
         # rename last file.
         if renameLastFile:
             imputCmd = 'rename "' + str(Path(inputPath).joinpath(lastFileName)) + '" "' + lastFileName[0:-4] + ' End' + newFileName[-4:] + '"'
+            recoverCmd = 'rename "' + str(Path(inputPath).joinpath(lastFileName[0:-4] + ' End' + newFileName[-4:])) + '" "' + lastFileOldName + '"'
+            cmdLog.append(imputCmd)
+            recoverLog.append(recoverCmd)
             print(lastFileName + '  ->  ' + lastFileName[0:-4] + ' End' + newFileName[-4:])
             os.system(imputCmd)
+        cmdLog.append('\n\n')
+        output = cmdLog + recoverLog
+        writefile(cmdLog + recoverLog)
         os.system("pause")
         
 if __name__ == '__main__':
