96c8beb16c2c4ea45c51204bba6c33b8f151331b
wangandi520 <wangandi520@gmail.com>
2021-06-17 14:07:56 +0800

    Update renameAnyFolderNameToFile.py

--- renameAnyFolderNameToFile.py
+++ renameAnyFolderNameToFile.py
@@ -3,31 +3,29 @@
 
 from pathlib import Path
 import sys
-import re
 import os
     
 def main(inputPath):
     # get folder name
-    folderName = Path(inputPath).name
-    if folderName.endswith('完]'):
-        loc = folderName.rfind('[')
-        folderName = folderName[:loc]
+    if Path.is_dir(Path(inputPath)):
+        folderName = Path(inputPath).name
+        if folderName.endswith('完]'):
+            loc = folderName.rfind('[')
+            folderName = folderName[:loc]
         
-    # rename file
-    fileCount = 0
-    lastFileName = ''
-    for file in Path(inputPath).glob('**/*'):
-        newFileName = folderName + 'Vol_' + str(fileCount + 1).zfill(2) + file.suffix
-        imputCmd = 'rename "' + str(Path(inputPath).joinpath(file)) + '" "' + newFileName + '"'
-        print(imputCmd)
-        os.system(imputCmd)
-        fileCount = fileCount + 1
-        lastFileName = newFileName
+        # rename file
+        fileCount = 0
+        lastFileName = ''
+        for file in Path(inputPath).glob('**/*'):
+            newFileName = folderName + 'Vol_' + str(fileCount + 1).zfill(2) + file.suffix
+            imputCmd = 'rename "' + str(Path(inputPath).joinpath(file)) + '" "' + newFileName + '"'
+            os.system(imputCmd)
+            fileCount = fileCount + 1
+            lastFileName = newFileName
             
-    # rename last file
-    imputCmd = 'rename "' + str(Path(inputPath).joinpath(lastFileName)) + '" "' + lastFileName[0:-4] + ' End' + newFileName[-4:] + '"'
-    print(imputCmd)
-    os.system(imputCmd)
+        # rename last file
+        imputCmd = 'rename "' + str(Path(inputPath).joinpath(lastFileName)) + '" "' + lastFileName[0:-4] + ' End' + newFileName[-4:] + '"'
+        os.system(imputCmd)
     
 if __name__ == '__main__':
     try:
