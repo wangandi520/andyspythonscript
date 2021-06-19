f5d95a1167e88c1750159bdedcab058b3bff09df
wangandi520 <wangandi520@gmail.com>
2021-06-19 13:43:48 +0800

    bug fix

--- （拖拽）交换文件或文件夹名前两个[]的内容（批量，多个文件或文件夹，文件名格式[][][][]XXX）.py
+++ （拖拽）交换文件或文件夹名前两个[]的内容（批量，多个文件或文件夹，文件名格式[][][][]XXX）.py
@@ -3,23 +3,20 @@
 
 from pathlib import Path
 import sys
-import os
-import re
     
 def main(inputPath):
     del inputPath[0]
-    for folder in inputPath:
-        folderName = Path(folder).name
-        nameParts = re.findall("(\\[[^\\]]*\\])", folderName)
-        lastSymbol = folderName.rfind(']') + 1
-        fileSuffix = folderName[lastSymbol:]
-        nameParts[1], nameParts[0] = nameParts[0], nameParts[1]
-        newFileName = ''
-        for part in nameParts:
-            newFileName = newFileName + part
-        newFileName = newFileName + fileSuffix
-        imputCmd = 'rename "' + folder + '" "' + newFileName + '"'
-        os.system(imputCmd)
+    for file in inputPath:
+        folderName = Path(file).name
+        leftSymbol = []
+        rightSymbol = []
+        for index in range(len(folderName)):
+            if folderName[index] == '[':
+                leftSymbol.append(index)
+            if folderName[index] == ']':
+                rightSymbol.append(index)
+        newFileName = folderName[leftSymbol[1]:rightSymbol[1] + 1] + folderName[leftSymbol[0]:rightSymbol[0] + 1] + folderName[rightSymbol[1] + 1:]
+        Path(file).rename(newFileName)
             
 if __name__ == '__main__':
     try:
