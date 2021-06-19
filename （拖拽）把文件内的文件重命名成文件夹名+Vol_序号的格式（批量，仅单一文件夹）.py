f8c7b8c24061fd8c3ae2f9940e5fb42e159ebe48
wangandi520 <wangandi520@gmail.com>
2021-06-19 08:44:15 +0800

    Update （拖拽）把文件内的文件重命名成文件夹名+Vol_序号的格式（批量，仅单一文件夹）.py

--- （拖拽）把文件内的文件重命名成文件夹名+Vol_序号的格式（批量，仅单一文件夹）.py
+++ （拖拽）把文件内的文件重命名成文件夹名+Vol_序号的格式（批量，仅单一文件夹）.py
@@ -83,9 +83,7 @@ def main(inputPath):
         
 if __name__ == '__main__':
     try:
-        if len(sys.argv) == 1:
-            main('.')
-        elif len(sys.argv) == 2:
-            main(sys.argv[1])
+        if len(sys.argv) >= 2:
+            main(sys.argv)
     except IndexError:
         pass
