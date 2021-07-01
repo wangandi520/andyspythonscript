e8337449e6e8674d5ea81760b3a81a2cb9ae5955
wangandi520 <wangandi520@gmail.com>
2021-07-01 22:40:18 +0800

    add new py

--- 识别作品名作者名并新建文件夹移动文件（拖拽，仅多个文件，可简繁转换，文件名格式[作品][作者]XX）.py
+++ 识别作品名作者名并新建文件夹移动文件（拖拽，仅多个文件，可简繁转换，文件名格式[作品][作者]XX）.py
@@ -1,18 +1,25 @@
 # encoding:utf-8
 # https://github.com/wangandi520/andyspythonscript
 
+from opencc import OpenCC
 from pathlib import Path
 import sys
 import re
        
 def main(inputPath):
     del inputPath[0]
+    # 文件名设置成简体 = True，繁体 = False
+    fileNameType = True
     for aPath in inputPath:
         if Path.is_file(Path(aPath)):
             fileName = Path(aPath).name
             nameParts = re.findall("(\\[[^\\]]*\\])", fileName)
-            workName = nameParts[0][1:-1]
-            authorName = nameParts[1][1:-1]
+            if fileNameType:
+                workName = OpenCC('t2s').convert(nameParts[0][1:-1])
+                authorName = OpenCC('t2s').convert(nameParts[1][1:-1])
+            else:
+                workName = OpenCC('s2t').convert(nameParts[0][1:-1])
+                authorName = OpenCC('s2t').convert(nameParts[1][1:-1])
             
             authorPath = Path(aPath).parent.joinpath(Path(authorName))
             if not authorPath.exists():
