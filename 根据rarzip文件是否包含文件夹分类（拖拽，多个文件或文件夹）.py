dc6f0b66230b4bb2384ca4be90bb1ea4fecfbdb5
wangandi520 <wangandi520@gmail.com>
2021-07-07 22:14:33 +0800

    support file and folder

--- 根据rarzip文件是否包含文件夹分类（拖拽，多个文件或文件夹）.py
+++ 根据rarzip文件是否包含文件夹分类（拖拽，多个文件或文件夹）.py
@@ -52,7 +52,50 @@ def main(inputPath):
                 Path(file).replace(withSubDirPath.joinpath(file.name))
             for file in noSubDir:
                 Path(file).replace(noSubDirPath.joinpath(file.name))
-
+        
+        if Path.is_file(Path(aPath)):
+            withSubDir = []
+            noSubDir = []
+            file = Path(aPath)
+            if rarfile.is_rarfile(file):
+                rf = rarfile.RarFile(file)
+                noDir = True
+                print(rf.namelist())
+                for eachFile in rf.infolist():
+                    if eachFile.is_dir():
+                        noDir = False
+                        if file not in withSubDir:
+                            withSubDir.append(file)
+                        break
+                if noDir and (file not in noSubDir):
+                    noSubDir.append(file)
+                rf.close()
+                                        
+            if zipfile.is_zipfile(file):
+                zf = zipfile.ZipFile(file)
+                noDir = True
+                for eachFile in zf.infolist():
+                    if eachFile.is_dir():
+                        noDir = False
+                        if file not in withSubDir:
+                            withSubDir.append(file)
+                        break
+                if noDir and (file not in noSubDir):
+                    noSubDir.append(file)
+                zf.close()
+            
+            withSubDirPath = Path(aPath).parent.joinpath(Path('包含文件夹'))
+            noSubDirPath = Path(aPath).parent.joinpath(Path('仅包含文件'))
+            if not withSubDirPath.exists() and withSubDir:
+                Path.mkdir(withSubDirPath)
+            if not noSubDirPath.exists() and noSubDir:
+                Path.mkdir(noSubDirPath)
+                
+            if withSubDir:
+                Path(file).replace(withSubDirPath.joinpath(file.name))
+            if noSubDir:
+                Path(file).replace(noSubDirPath.joinpath(file.name))
+        
 if __name__ == '__main__':
     try:
         if len(sys.argv) >= 2:
