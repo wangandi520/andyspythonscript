b245a1b8598820405c0e3ced0e53e0ade34580a5
wangandi520 <wangandi520@gmail.com>
2021-07-23 13:26:02 +0800

    new py

---
+++ 根据漫画补档网址，生成帖子名.txt包含网址发布时间一楼内容.py
@@ -0,0 +1,29 @@
+# encoding:utf-8
+# https://github.com/wangandi520/andyspythonscript
+# pip3 install requests
+# pip3 install bs4
+
+import requests
+from bs4 import BeautifulSoup
+
+def writefile(fileName, filereadlines):
+    #write file
+    with open(fileName + '.txt', mode='w', encoding='UTF-8') as newfile:
+        newfile.writelines(filereadlines)
+
+def main():
+    url = 'https://www.manhuabudang.com/read.php?tid=xxx'
+    getHtml = requests.get(url)
+    soup = BeautifulSoup(getHtml.text, 'html.parser')
+    getName = soup.find(attrs={'id' : 'subject_tpc'}).string
+    getTime = '\n\n' + soup.find(attrs={'id' : 'readfloor_tpc'}).find(attrs={'class' : 'tipTop s6'}).findAll('span')[-1].get_text() + '\n'
+    getContent = str(soup.find(attrs={'id' : 'read_tpc'}))
+    getContent = getContent.replace('<br/>',"\n")
+    getContent = BeautifulSoup(getContent, 'html.parser').get_text()
+    
+    writefile(getName, url + getTime + getContent)
+        
+
+if __name__ == '__main__':
+    main()
+    
