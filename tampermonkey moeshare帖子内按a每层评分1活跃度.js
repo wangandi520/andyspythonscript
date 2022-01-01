aa82f47d948df29815dce00413aa50f3db00184b
wangandi520 <wangandi520@gmail.com>
2022-01-02 00:35:08 +0800

    Update tampermonkey moeshare帖子内按a每层评分1活跃度.js

--- tampermonkey moeshare帖子内按a每层评分1活跃度.js
+++ tampermonkey moeshare帖子内按a每层评分1活跃度.js
@@ -10,28 +10,31 @@
 // ==/UserScript==
 
 
-var i = -1;
+
+function pingfen(eachScore,i,count){
+    eachScore[i - 1].click();
+    console.log('正在评分第' + i + '楼');
+    setTimeout(function(){
+        var my = document.querySelector('#c_model');
+        my.querySelector('select').selectedIndex = 1;
+        my.querySelector('input').value = 1;
+        setTimeout(function(){
+            document.querySelector('#box_container .btn2 button').click();
+            count = count + 1;
+            console.log('评分完毕，活跃度 +' + count);
+        }, 1000);
+    }, 1000);
+}
+
 document.onkeydown = function(){
     var eachScore = document.getElementsByClassName('r-score');
+    var i = 1;
+    var count = 0;
+    //在帖子内，按a键开始自动评分
     if (event.keyCode == 65){
-        setTimeout(
-            function()
-            {
-                eachScore[i].click();
-                console.log('正在评分第' + i + '楼');
-                setTimeout(
-                    function()
-                    {
-                        var my = document.querySelector('#c_model');
-                        my.querySelector('select').selectedIndex = 1;
-                        my.querySelector('input').value = 1;
-                        setTimeout(
-                            function()
-                            {
-                                document.querySelector('#box_container .btn2 button').click();
-                            }, 1000);
-                    }, 1000);
-            }, 1000);
+        setInterval(function(){
+            pingfen(eachScore,i,count);
+            i = i + 1;
+        }, 5000);
     }
-i = i + 1;
 }
