31872ad47796a92f46d159d386b94ac3147254ca
wangandi520 <wangandi520@gmail.com>
2022-02-04 09:33:19 +0800

    Update getmyip.py

--- getmyip.py
+++ getmyip.py
@@ -9,19 +9,19 @@ se = requests.Session()
 if response.status_code == 200:
 	newip = response.text
 
-with open('ip.txt', mode='r', encoding = 'UTF-8') as file:
+with open('/srv/git/getmyip/ip.txt', mode='r', encoding = 'UTF-8') as file:
 	read = file.readlines()
 	file.close()
 	oldip = read[0]
 
 if newip != oldip:
-	newfile = open('ip.txt', mode = 'w', encoding = 'UTF-8')
+	newfile = open('/srv/git/getmyip/ip.txt', mode = 'w', encoding = 'UTF-8')
 	newfile.writelines(newip)
 	newfile.close()
-	mygit = git.Git('.')
-	mygit.add('ip.txt')
+	mygit = git.Git('/srv/git/getmyip')
+	mygit.add('/srv/git/getmyip/ip.txt')
 	mygit.commit('-m ' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
 	mygit.push()
 if newip == oldip:
-	mygit = git.Git('.')
+	mygit = git.Git('/srv/git/getmyip')
 	mygit.push()
