bef9d5b91604961e54f6b416fca62c46a6d06528
wangandi520 <wangandi520@gmail.com>
2022-02-17 20:38:58 +0800

    Create tampermonkey 漫画补档帖子回帖用户名在浏览器f12中显示.js

---
+++ tampermonkey 漫画补档帖子回帖用户名在浏览器f12中显示.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         漫画补档帖子回帖用户名在浏览器f12中显示
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.manhuabudang.com/*
+// @icon         https://www.google.com/s2/favicons?domain=manhuabudang.com
+// @grant        none
+// ==/UserScript==
+
+var name = document.getElementsByClassName('readName b');
+
+
+for (var i = 0, len = name.length; i < len; i++) {
+    console.log(name[i].children[1].innerHTML);
+}
