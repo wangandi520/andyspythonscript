949a0ac0396f118e64599d67babeccb954918fee
wangandi520 <wangandi520@gmail.com>
2022-03-02 08:38:20 +0800

    Create 给文件原扩展名后添加新扩展名，rar为例（拖拽，多个文件或文件夹）.py

---
+++ 给文件原扩展名后添加新扩展名，rar为例（拖拽，多个文件或文件夹）.py
@@ -0,0 +1,26 @@
+# encoding:utf-8
+# https://github.com/wangandi520/andyspythonscript
+
+from pathlib import Path
+import sys
+    
+def main(inputPath):
+    # 新扩展名
+    newSuffix = '.rar'
+    
+    del inputPath[0]
+    for aPath in inputPath:
+        if Path.is_dir(Path(aPath)):
+            for file in Path(aPath).glob('*'):
+                if Path.is_file(file) and file.suffix != newSuffix:
+                    file.rename(Path(aPath).joinpath(file.name + newSuffix))
+        if Path.is_file(Path(aPath)) and Path(aPath).suffix != newSuffix:
+            print(Path(aPath).suffix)
+            Path(aPath).rename(Path(aPath).parent.joinpath(Path(aPath).name + newSuffix))
+        
+if __name__ == '__main__':
+    try:
+        if len(sys.argv) >= 2:
+            main(sys.argv)
+    except IndexError:
+        pass
