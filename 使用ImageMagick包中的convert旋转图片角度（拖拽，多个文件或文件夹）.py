7ff9954a830e9a195888bbcd9bc9a86dbf3bff63
wangandi520 <wangandi520@gmail.com>
2022-04-29 11:36:44 +0800

    update

---
+++ 使用ImageMagick包中的convert旋转图片角度（拖拽，多个文件或文件夹）.py
@@ -0,0 +1,38 @@
+# encoding:utf-8
+# https://github.com/wangandi520/andyspythonscript
+
+from pathlib import Path
+import sys
+import os.path
+
+# https://imagemagick.org/
+# 需要ImageMagick包中的convert.exe
+# ImageMagick-7.1.0-portable-Q16-HDRI-x64.zip
+# convert -rotate 90 jb.jpg 90.jpg
+
+def main(inputPath):
+    # 设置文件类型
+    #fileType = ['.png']
+    fileType = ['.png','.jpg']
+    # 旋转角度，90 = 顺时针90度
+    setRotate = 90
+    
+    del inputPath[0]
+    for aPath in inputPath:
+        if Path.is_dir(Path(aPath)):
+            for file in Path(aPath).glob('**/*'):
+                if file.suffix in fileType:
+                    cmd = 'convert.exe -rotate ' + str(setRotate) + ' "' + str(file) + '" "' + str(file) + '"'
+                    os.system(cmd)
+                
+        if Path.is_file(Path(aPath)):
+            if Path(aPath).suffix in fileType:
+                cmd = 'convert.exe -rotate ' + str(setRotate) + ' "' + aPath + '" "' + aPath + '"'
+                os.system(cmd)
+
+if __name__ == '__main__':
+    try:
+        if len(sys.argv) >= 2:
+            main(sys.argv)
+    except IndexError:
+        pass
--- 使用pingo压缩图片（拖拽，多个文件或文件夹）.py
+++ 使用pingo压缩图片（拖拽，多个文件或文件夹）.py
@@ -1,34 +1,35 @@
 # encoding:utf-8
 # https://github.com/wangandi520/andyspythonscript
 
-from PIL import Image
 from pathlib import Path
 import sys
 import os.path
   
-# 需要ImageMagick包中的convert.exe
-# ImageMagick-7.1.0-portable-Q16-HDRI-x64.zip
-# convert -rotate 90 jb.jpg 90.jpg
+# https://css-ig.net/bin/pingo-win64.zip
+# usage (examples)
+# pingo [options] files/folders pingo -s0 *.png pingo -s0 myfolder pingo -s0 c:\myfolder
+# PNG — lossless
+# pingo -sN file.png
+# N from 0 to 9, optimization level
 
 def main(inputPath):
     # 设置文件类型
     #fileType = ['.png']
     fileType = ['.png','.jpg']
-    # 旋转角度，90 = 顺时针90度
-    setRotate = 90
     
     del inputPath[0]
     for aPath in inputPath:
         if Path.is_dir(Path(aPath)):
-            allFileArray = []
             for file in Path(aPath).glob('**/*'):
                 if file.suffix in fileType:
-                    cmd = 'convert.exe -rotate ' + str(setRotate) + ' ' + str(file) + ' ' + str(file)
+                    print('pingo.exe -s9 "' + str(file) + '"')
+                    cmd = 'pingo.exe -s9 "' + str(file) + '"'
                     os.system(cmd)
                 
         if Path.is_file(Path(aPath)):
             if Path(aPath).suffix in fileType:
-                cmd = 'convert.exe -rotate ' + str(setRotate) + ' ' + aPath + ' ' + aPath
+                print('pingo.exe -s9 "' + aPath)
+                cmd = 'pingo.exe -s9 "' + aPath + '"'
                 os.system(cmd)
 
 if __name__ == '__main__':
