9795b4881b88de29025779eab272ef9ab47f7af5
wangandi520 <wangandi520@gmail.com>
2022-04-30 10:05:00 +0800

    new

--- 重命名文件夹的最后一个文件（拖拽，递归，多个文件夹）.py
+++ 重命名文件夹的最后一个文件（拖拽，递归，多个文件夹）.py
@@ -5,9 +5,9 @@ from pathlib import Path
 import sys
     
 def main(inputPath):
-
-    # 新文件名
-    newFileName = '0001.jpg'
+    # 新文件名，不修改扩展名
+    newFileName = '0001'
+    
     del inputPath[0]
     
     for aPath in inputPath:
@@ -16,7 +16,7 @@ def main(inputPath):
             for file in Path(aPath).glob('**/*'):
                 if Path.is_file(file):
                     allFilePath.append(file)
-            allFilePath[-1].rename(allFilePath[-1].parent.joinpath(newFileName))
+            allFilePath[-1].rename(allFilePath[-1].parent.joinpath(newFileName + allFilePath[-1].suffix))
                 
 if __name__ == '__main__':
     try:
