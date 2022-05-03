8099cc85b54282e9d6f8fd8e374a6dec2ed87829
wangandi520 <wangandi520@gmail.com>
2022-05-03 18:03:59 +0800

    Update 使用ImageMagick包中的convert把每两张图片水平合并（拖拽，仅文件夹）.py

--- 使用ImageMagick包中的convert把每两张图片水平合并（拖拽，仅文件夹）.py
+++ 使用ImageMagick包中的convert把每两张图片水平合并（拖拽，仅文件夹）.py
@@ -15,6 +15,8 @@ def main(inputPath):
     # 设置文件类型
     #fileType = ['.png']
     fileType = ['.png','.jpg']
+    # 从右到左 = True, 从左到右 = False
+    direction = True
     
     del inputPath[0]
     for aPath in inputPath:
@@ -23,9 +25,15 @@ def main(inputPath):
             for file in Path(aPath).glob('**/*'):
                 if file.suffix in fileType:
                     allFilesPath.append(file)
+            newPath = Path(aPath).joinpath('convertedImageFolder')
+            if not newPath.exists():
+                Path.mkdir(newPath)
             for imgIndex in range(0, len(allFilesPath), 2):
                 if Path.is_file(allFilesPath[imgIndex]) and Path.is_file(allFilesPath[imgIndex + 1]) and (allFilesPath[imgIndex].suffix == allFilesPath[imgIndex + 1].suffix):
-                    cmd = 'convert.exe +append "' + str(allFilesPath[imgIndex]) + '" "' + str(allFilesPath[imgIndex + 1]) + '" "' + str(Path(aPath).joinpath(allFilesPath[imgIndex].stem + '_' + allFilesPath[imgIndex + 1].stem + allFilesPath[imgIndex].suffix)) + '"'
+                    if direction:
+                        cmd = 'convert.exe +append "' + str(allFilesPath[imgIndex + 1]) + '" "' + str(allFilesPath[imgIndex]) + '" "' + str(Path(newPath).joinpath(allFilesPath[imgIndex].stem + '_' + allFilesPath[imgIndex + 1].stem + allFilesPath[imgIndex].suffix)) + '"'
+                    else:
+                        cmd = 'convert.exe +append "' + str(allFilesPath[imgIndex]) + '" "' + str(allFilesPath[imgIndex + 1]) + '" "' + str(Path(newPath).joinpath(allFilesPath[imgIndex].stem + '_' + allFilesPath[imgIndex + 1].stem + allFilesPath[imgIndex].suffix)) + '"'
                     print(cmd)
                     os.system(cmd)
                
