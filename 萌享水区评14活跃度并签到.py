91c8cc9596c5bdf3e8fad9037b6a4303089b57da
wangandi520 <wangandi520@gmail.com>
2022-08-20 22:16:50 +0800

    Update 萌享水区评14活跃度并签到.py

--- 萌享水区评14活跃度并签到.py
+++ 萌享水区评14活跃度并签到.py
@@ -7,13 +7,12 @@ import json
 import calendar
 
 # 本脚本使用前提：
-# 1.安装python和pip插件，pip install requests，用不了的话搜索pip教程
-# 2.能进入萌享会员自由交流区https://moeshare.cc/thread-htm-fid-16.html
-# 不能进的话每天14活跃度签到，月底道具中心买全满勤奖后使用
+# 1.网上搜索，安装python和pip插件，在命令提示符输入pip install requests
+# 2.账号能进入萌享会员自由交流区https://moeshare.cc/thread-htm-fid-16.html，不能进的话每天14活跃度签到，月底道具中心买全满勤奖后使用
 # 3.cookie设置：
-# 需要填写下面的myCookie和myAgent
-# 萌享首页，chrome或edge按f12，网络，刷新页面，名称里选index.php，右侧请求标头，右键user-agent和cookie，复制值到下面myCookie和myAgent后，别忘了引号。
-# f12查看自己的cookie并修改，只需复制8017a_c_stamp=前面的部分，例：
+# 只需要填写下面的myCookie和myAgent，其他的不用改
+# 萌享首页，chrome或edge按f12，网络，刷新页面，名称里选index.php，右侧请求标头，右键cookie和user-agent，复制值到下面myCookie和myAgent后，别忘了引号。
+# myCookie只需复制8017a_c_stamp=前面的部分，例：
 # somecookie = "8017a_ck_info=/	; 8017a_jobpop=1; 8017a_threadlog=,6,4,16,22,2,; 8017a_ckquestion=asdfaawgawegawegawegawegwaegawegawegawegawe; zh_choose=n; 8017a_winduser=awegwaegawegwaegawegawegawegawegwaeg; 8017a_ipstate=1658973831; 8017a_readlog=,241573,241587,; 8017a_c_stamp="
 # someAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Safari/537.36 Edg/103.0.1264.70"
 
