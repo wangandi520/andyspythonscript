d33e1b476ec095680c3b8426204b50b1e2085758
wangandi520 <wangandi520@gmail.com>
2022-08-22 21:40:19 +0800

    update

--- 萌享水区评14活跃度并签到.py
+++ 萌享水区评14活跃度并签到.py
@@ -15,12 +15,11 @@ import calendar
 # 只需要填写下面的myCookie和myAgent，其他的不用改
 # 萌享首页，chrome或edge按f12，网络，刷新页面，名称里选index.php，右侧请求标头，右键cookie和user-agent，复制值到下面myCookie和myAgent后，别忘了引号。
 # myCookie只需复制8017a_c_stamp=前面的部分，例：
-# somecookie = "8017a_ck_info=/	; 8017a_jobpop=1; 8017a_threadlog=,6,4,16,22,2,; 8017a_ckquestion=asdfaawgawegawegawegawegwaegawegawegawegawe; zh_choose=n; 8017a_winduser=awegwaegawegwaegawegawegawegawegwaeg; 8017a_ipstate=1658973831; 8017a_readlog=,241573,241587,; 8017a_c_stamp="
+# someCookie = "8017a_ck_info=/	; 8017a_jobpop=1; 8017a_threadlog=,6,4,16,22,2,; 8017a_ckquestion=asdfaawgawegawegawegawegwaegawegawegawegawe; zh_choose=n; 8017a_winduser=awegwaegawegwaegawegawegawegawegwaeg; 8017a_ipstate=1658973831; 8017a_readlog=,241573,241587,; 8017a_c_stamp="
 # someAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Safari/537.36 Edg/103.0.1264.70"
 
 myCookie = ";;;;8017a_c_stamp="
-
-myAgent = "Edg/103.0.1264.71"
+myAgent = "Edg/103.0.1264.70"
 
 # 水区帖子tid
 tid = '211701'
