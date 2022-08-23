b80faba2ba5a313b462ed2db7d52e3a06955ea2e
wangandi520 <wangandi520@gmail.com>
2022-08-23 22:52:34 +0800

    Update 使用ImageMagick包中的convert把图片水平分割成两张图（拖拽，多个文件或文件夹）.py

--- 使用ImageMagick包中的convert把图片水平分割成两张图（拖拽，多个文件或文件夹）.py
+++ 使用ImageMagick包中的convert把图片水平分割成两张图（拖拽，多个文件或文件夹）.py
@@ -10,33 +10,36 @@ import os
 # 需要ImageMagick包中的convert.exe
 # ImageMagick-7.1.0-portable-Q16-HDRI-x64.zip
 # convert old.jpg -crop 128x128 new.jpg
+# https://imagemagick.org/script/command-line-options.php#crop
 
-def main(inputPath):
-    # 设置文件类型
-    #fileType = ['.png']
-    fileType = ['.png','.jpg']
+def doCrop(filePath):
+    # 设置图片质量，越大体积越大，1到100，质量85体积差不多容差20，质量100三倍体积容差5
+    setQuality = 85
     # 设置新图片的文件名结尾
     # 左页
     leftImgName = '_left'
     # 右页
     rightImgName = '_right'
+    img = Image.open(filePath)
+    imgWidth, imgHeight = img.size
+    cmd = 'convert.exe "' + str(filePath) + '" -crop ' + str(imgWidth / 2) + 'x' + str(imgHeight) + '  -quality ' + str(setQuality) + ' "' + str(filePath) + '"'
+    print(cmd)
+    os.system(cmd)
+    
+def main(inputPath):
+    # 设置文件类型
+    fileType = ['.png','.jpg']
     
     del inputPath[0]
     for aPath in inputPath:
         if Path.is_dir(Path(aPath)):
             for file in Path(aPath).glob('**/*'):
                 if file.suffix.lower() in fileType:
-                    img = Image.open(file)
-                    imgWidth, imgHeight = img.size
-                    cmd = 'convert.exe "' + str(file) + '" -crop ' + str(imgWidth / 2) + 'x' + str(imgHeight) + ' "' + str(file) + '"'
-                    os.system(cmd)
+                    doCrop(file)
                 
         if Path.is_file(Path(aPath)):
             if Path(aPath).suffix.lower() in fileType:
-                img = Image.open(Path(aPath))
-                imgWidth, imgHeight = img.size
-                cmd = 'convert.exe "' + aPath + '" -crop ' + str(imgWidth / 2) + 'x' + str(imgHeight) + ' "' + aPath + '"'
-                os.system(cmd)
+                doCrop(Path(aPath))
 
 if __name__ == '__main__':
     try:
