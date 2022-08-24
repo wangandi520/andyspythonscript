e78a4a22a6542556e1824c57dbfad2d386be9888
wangandi520 <wangandi520@gmail.com>
2022-08-24 10:33:59 +0800

    Update 萌享刷在线时间.py

--- 萌享刷在线时间.py
+++ 萌享刷在线时间.py
@@ -9,7 +9,7 @@ import schedule
 # 2.cookie设置：
 # 只需要填写下面的myCookie和myAgent，如果无法访问url，可以试着改成其他帖子的地址，其他的不用改
 # 萌享首页，chrome或edge按f12，网络，刷新页面，名称里选index.php，右侧请求标头，右键cookie和user-agent，复制值到下面myCookie和myAgent后，别忘了引号。
-# myCookie只需复制8017a_readlog=前面的部分，例：
+# myCookie只需复制8017a_c_stamp=前面的部分，例：
 # someCookie = "8017a_ck_info=/	; 8017a_jobpop=1; 8017a_threadlog=,6,4,16,22,2,; 8017a_ckquestion=asdfaawgawegawegawegawegwaegawegawegawegawe; zh_choose=n; 8017a_winduser=awegwaegawegwaegawegawegawegawegwaeg; 8017a_ipstate=1658973831; 8017a_c_stamp="
 # someAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Safari/537.36 Edg/103.0.1264.70"
 # 可能脚本停止一段时间后才会计入在线时间
@@ -17,9 +17,9 @@ import schedule
 def doOnline():
     global myCount
     
-    myCookie = "8017a_ck_info=/	; 8017a_jobpop=1; 8017a_threadlog=,6,4,16,22,2,; 8017a_ckquestion=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx; zh_choose=n; 8017a_winduser=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx; 8017a_ipstate=1658973831; 8017a_c_stamp="
+    myCookie = "8017a_c_stamp="
 
-    myAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Safari/537.36 Edg/103.0.1264.71"
+    myAgent = "Edg/103.0.1264.70"
 
     # 帖子地址
     url = 'https://moeshare.cc/read-htm-tid-211701.html'
