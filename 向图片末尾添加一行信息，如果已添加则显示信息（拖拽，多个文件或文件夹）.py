054749c2b78f1ec2ba27f89470abbf48d9facd7e
wangandi520 <wangandi520@gmail.com>
2022-08-27 23:05:16 +0800

    Create 向图片末尾添加一行信息，如果已添加则显示信息（拖拽，多个文件或文件夹）.py

---
+++ 向图片末尾添加一行信息，如果已添加则显示信息（拖拽，多个文件或文件夹）.py
@@ -0,0 +1,50 @@
+# encoding:utf-8
+# https://github.com/wangandi520/andyspythonscript
+
+from pathlib import Path
+import sys
+
+def doAddMessageToImage(filePath):
+    # typeof(filePath): Path
+    # 设置文件类型
+    fileType = ['.png','.jpg']
+    # 需要添加的信息
+    myMessage = b'myMessage'
+    if filePath.suffix.lower() in fileType:
+        with open(filePath, 'rb') as fileRead:
+            fileReadLine = fileRead.readlines()
+    if filePath.suffix.lower() == '.jpg':
+        if fileReadLine[-1][-2:] == b'\xff\xd9':
+            fileReadLine.append(b'\n')
+            fileReadLine.append(myMessage)
+            with open(filePath.parent.joinpath(filePath.stem + '_new' + filePath.suffix), 'wb') as fileSave:
+                fileSave.writelines(fileReadLine)
+        else:
+            print(str(fileReadLine[-1], encoding = 'utf-8'))
+    if filePath.suffix.lower() == '.png':
+        if fileReadLine[-1][-1:] == b'\x82':
+            fileReadLine.append(b'\n')
+            fileReadLine.append(myMessage)
+            with open(filePath.parent.joinpath(filePath.stem + '_new' + filePath.suffix), 'wb') as fileSave:
+                fileSave.writelines(fileReadLine)
+        else:
+            print(str(fileReadLine[-1], encoding = 'utf-8'))
+
+def main(inputPath):
+    del inputPath[0]
+    for aPath in inputPath:
+        if Path.is_dir(Path(aPath)):
+            for file in Path(aPath).glob('**/*'):
+                doAddMessageToImage(file)
+                
+        if Path.is_file(Path(aPath)):
+            doAddMessageToImage(Path(aPath))
+
+    input('按回车键退出')
+
+if __name__ == '__main__':
+    try:
+        if len(sys.argv) >= 2:
+            main(sys.argv)
+    except IndexError:
+        pass
