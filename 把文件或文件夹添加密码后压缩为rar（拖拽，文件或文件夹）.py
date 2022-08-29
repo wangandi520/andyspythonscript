c34e8dc7c8ea19e24864eeee3e8d8322f2463acd
wangandi520 <wangandi520@gmail.com>
2022-08-29 22:26:27 +0800

    Update 把文件或文件夹添加密码后压缩为rar（拖拽，文件或文件夹）.py

--- 把文件或文件夹添加密码后压缩为rar（拖拽，文件或文件夹）.py
+++ 把文件或文件夹添加密码后压缩为rar（拖拽，文件或文件夹）.py
@@ -11,16 +11,16 @@ def doAddToEncryptedRar(filePath):
     # 设置密码为1234
     myPassword = '1234'
     # 新文件名 = 密码1234_ + 旧文件名
-    newFileName = '密码' + myPassword + '_' + Path(filePath).stem
+    newFileName = '密码' + myPassword + '_' + Path(filePath).stem + '.rar'
     print('正在处理...' + filePath.name)
     # -ep不包含路径
     # -ep1包含一层目录
-    # m0压缩方式存储
+    # -m0压缩方式存储
     # -hp加密
-    if Path.is_file(filePath):   
-        os.system('Rar.exe -ep -m0 -hp' + myPassword + ' a ' + str(filePath.parent.joinpath(newFileName + '.rar')) + ' ' + str(filePath))
+    if Path.is_file(filePath):
+        os.system('Rar.exe -ep -m0 -hp' + myPassword + ' a "' + str(filePath.parent.joinpath(newFileName)) + '" "' + str(filePath) + '"')
     if Path.is_dir(filePath):
-        os.system('Rar.exe -ep1 -m0 -hp' + myPassword + ' a ' + str(filePath.parent.joinpath(newFileName + '.rar')) + ' ' + str(filePath))
+        os.system('Rar.exe -ep1 -m0 -hp' + myPassword + ' a "' + str(filePath.parent.joinpath(newFileName)) + '" "' + str(filePath) + '"')
     print('完成')
             
 def main(inputPath):
