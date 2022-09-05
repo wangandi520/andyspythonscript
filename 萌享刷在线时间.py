d61b604a0304679bb9b642eb4a39c7b67d5da912
wangandi520 <wangandi520@gmail.com>
2022-09-05 16:28:35 +0800

    fix moe

---
+++ 萌享刷在线时间.py
@@ -0,0 +1,66 @@
+# encoding:utf-8
+
+import requests
+import time
+import schedule
+
+# 本脚本使用前提：
+# 1.网上搜索，安装python和pip插件，在命令提示符输入pip install schedule requests
+# 2.cookie设置：
+# 只需要填写下面的myCookie和myAgent，如果无法访问url，可以试着改成其他帖子的地址，其他的不用改
+# 萌享首页，chrome或edge按f12，网络，刷新页面，名称里选index.php，右侧请求标头，右键cookie和user-agent，复制值到下面myCookie和myAgent后，别忘了引号。
+# myCookie只需复制8017a_c_stamp=前面的部分，例：
+# someCookie = "8017a_ck_info=/	; 8017a_jobpop=1; 8017a_threadlog=,6,4,16,22,2,; 8017a_ckquestion=xxxxxxxxxxxxxxxxxxxxxxx; zh_choose=n; 8017a_winduser=xxxxxxxxxxxxxxxxxxxxxxx; 8017a_ipstate=1658973831; 8017a_c_stamp="
+# someAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.131 Safari/537.36 Edg/103.0.1264.70"
+# 可能脚本停止一段时间后才会计入在线时间
+
+global myCookie
+global myAgent
+global myCount
+global firstTime
+global timeInterval
+
+# 几分钟刷新一次
+timeInterval = 5
+
+myCount = 0
+
+myCookie = "8017a_c_stamp="
+
+myAgent = "Edg/103.0.1264.70"
+
+def doOnline():
+    global myCount
+    # 帖子地址
+    url = 'https://moeshare.cc/read-htm-tid-211701-page-2000.html'
+
+    headers = {"User-Agent": myAgent,
+               "Cookie": myCookie + str(int(round(time.time()))) + "; 8017a_lastpos=index;  8017a_lastvisit=566	" + str(int(round(time.time())) - 300) + "	/index.php"}
+                
+    session = requests.session()
+    response = session.post(url, headers=headers)
+    if response.status_code == 200:
+        print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+        print('未转存时间' + str(firstTime) + '分钟，第' + str(myCount) + '次刷新，共' + str(myCount * timeInterval) + '分钟，预计总时间' + str(firstTime + myCount * timeInterval) + '分钟')
+        myCount = myCount + 1
+
+# 获得未转存时间
+url = 'https://moeshare.cc/hack.php?H_name=integral'
+headers = {"User-Agent": myAgent,
+           "Cookie": myCookie + str(int(round(time.time()))) + "; 8017a_lastpos=index;  8017a_lastvisit=566	" + str(int(round(time.time())) - 300) + "	/index.php"}
+session2 = requests.session()
+response2 = session2.post(url, headers=headers)
+if response2.status_code == 200:
+    tmpIndex01 = (response2.text).find('<td>未转存时间:</td>')
+    tmpIndex02 = (response2.text).find('<td>这次可以保存的积分:</td>')
+    firstTime = int(response2.text[tmpIndex01 + 20: tmpIndex02 - 36])
+time.sleep(3)
+
+# 第一次执行
+doOnline()
+# 设置间隔时间5分钟
+schedule.every(timeInterval).minutes.do(doOnline)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
--- 萌享水区评10活跃度并签到.py
+++ 萌享水区评10活跃度并签到.py
@@ -10,12 +10,13 @@ import math
 # 本脚本原理：
 # 使用cookie模拟登录，给帖子每层楼评分1活跃度，自己也获得1活跃度，10活跃度后签到
 # 20220831，论坛改版，lv0,1每天能加的活跃度是2,5，所以现在需要用其他方式补充，lv2刚好每天10点
-# 新脚本会把在线时间转换成缺少的活跃度，如果你是lv1，可以加5点活跃，需要有5以上的在线时间才能签到成功
+# 新脚本会把时辰转换成缺少的活跃度，如果你是lv1，可以加5点活跃，需要有3以上的时辰才能签到成功
 # 保存时间：https://moeshare.cc/hack.php?H_name=integral，换成活跃度：https://moeshare.cc/userpay.php?action=change
 # 本脚本使用前提：
 # 1.网上搜索，安装python和pip插件，在命令提示符输入pip install requests
 # 2.账号能进入萌享会员自由交流区https://moeshare.cc/thread-htm-fid-16.html，每天10活跃度签到，月底道具中心买全满勤奖后使用
-# 3.cookie设置：
+# 3.确定签到步骤，30和32行
+# 4.cookie设置：
 # 只需要填写下面的myCookie和myAgent，其他的不用改
 # 萌享首页，chrome或edge按f12，网络，刷新页面，名称里选index.php，右侧请求标头，右键cookie和user-agent，复制值到下面myCookie和myAgent后，别忘了引号。
 # myCookie只需复制8017a_c_stamp=前面的部分，例：
@@ -25,6 +26,11 @@ import math
 myCookie = "8017a_c_stamp="
 
 myAgent = "Edg/103.0.1264.70"
+
+# 是否回复一贴，回复一次+1活跃度，是 = True，否 = False
+ifReply = True
+# 是否把时辰转换成缺少活跃度，1时辰 = 2活跃度，是 = True，否 = False
+ifOnlineTimeToHuoyue = True
 
 # 水区帖子tid
 tid = '211701'
@@ -64,15 +70,16 @@ if response5.status_code == 200:
     tmpIndex5 = (response5.text).find('<li class="w mb5">活跃度')
     tmpIndex6 = (response5.text).find('<li class="w mb5">YQBD')
     tmpIndex7 = (response5.text).find('<li class="w mb5">本月打卡')
-    tmpIndex8 = (response5.text).find('<li class="w mb5">在线时间')
+    tmpIndex8 = (response5.text).find('<li class="w mb5">时辰')
     tmpIndex9 = (response5.text).find('<h5 class="h5"><a class="fr" href="userpay.php?action=log">进入积分日志')
     getHuoyueNum = response5.text[tmpIndex5 + 18: tmpIndex6 - 6]
     daka = response5.text[tmpIndex7 + 23: tmpIndex5 - 6]
-    onlineTime = response5.text[tmpIndex8 + 23: tmpIndex9 - 12]
+    onlineTime = response5.text[tmpIndex8 + 21: tmpIndex9 - 12]
     print('本月打卡: ' + daka)
-    print('在线时间: ' + onlineTime)
+    print('时辰: ' + onlineTime)
     try:
         huoyueAddedByScript = int(getHuoyueNum.split('：')[1])
+        print('现在活跃度：' + str(huoyueAddedByScript))
     except IndexError:
         print('也许cookie设置错误。01')
         sys.exit()
@@ -91,7 +98,7 @@ lastDay = calendar.monthrange(year, month)[1]
 if (daka == str(today)):
     print('今天已经打过卡了。')
 elif (huoyueAddedByScript >= 10):
-    print('活跃 >= 10')
+    print('活跃度 >= 10')
 elif (daka != time.strftime("%d", time.localtime())):
     headers2 = {"User-Agent": myAgent,
            "Cookie": myCookie + str(int(round(time.time()))) + "; 8017a_lastvisit=0	" + str(int(round(time.time()))) + "	/index.php"}
@@ -148,7 +155,7 @@ elif (daka != time.strftime("%d", time.localtime())):
     elif (index != -1):
         for eachPid in pidArray:
             newtime2 = int(round(time.time() * 1000))
-            myreferer = 'https://moeshare.cc/read-htm-tid-211701-page-' + newpage + '.html'
+            myreferer = 'https://moeshare.cc/read-htm-tid-' + tid + '-page-' + newpage + '.html'
             shuiquUrl = 'https://moeshare.cc/operate.php?action=showping&ajax=1&nowtime=' + \
                 str(newtime2) + '&verify=' + myveri
             preCookie = myCookie.split('8017a_c_stamp')[0]
@@ -161,7 +168,7 @@ elif (daka != time.strftime("%d", time.localtime())):
             formData = {
                 "verify": myveri,
                 "page": newpage,
-                "tid": "211701",
+                "tid": tid,
                 "selid[]": eachPid,
                 "step": "1",
                 "cid[]": "currency",
@@ -179,46 +186,85 @@ elif (daka != time.strftime("%d", time.localtime())):
                     break;
                 else:
                     print('页数: ' + str(newpage) + ' PID: ' +
-                          str(eachPid) + ' Error: ' + response2.text)
+                          str(eachPid) + ' Error: ' + response2.text)          
             time.sleep(3)
-            
-    # 在线时间转换成活跃度
-    saveOnlineTimeUrl = 'https://moeshare.cc/hack.php?H_name=integral&action=save'
+        
+        print('现在活跃度：' + str(huoyueAddedByScript))
+    
+    # 时辰转换成活跃度
+    if ifOnlineTimeToHuoyue:
+        saveOnlineTimeUrl = 'https://moeshare.cc/hack.php?H_name=integral&action=save'
+        formData3 = {
+            "integralsubmit": '1',
+            "Submit": '保存积分'
+        }
+        headers = {"User-Agent": myAgent,
+               "Cookie": myCookie + str(int(round(time.time()))) + "; 8017a_lastvisit=0	" + str(int(round(time.time()))) + "	/index.php"}
+        response12 = requests.post(saveOnlineTimeUrl, headers=headers, data=formData3)
+        if response12.status_code == 200:
+            if ('可以保存的积分小于1') in response12.text:
+                print('可以保存的现在时间小于1')
+            elif ('一共转存了'):
+                print('转存成功')
+        time.sleep(3)
+
+        if int(onlineTime) / 2 + 1 > 10 - huoyueAddedByScript:
+            onlineTimeToHuoyueUrl = 'https://moeshare.cc/userpay.php?action=change&'
+            transOnTime = str(math.ceil((10 - huoyueAddedByScript) / 2))
+            formData2 = {
+                "step": '3',
+                "type": '3_currency',
+                "verify": myveri,
+                "change": transOnTime
+            }
+            print(transOnTime + '时辰转换成活跃度')
+            headers = {"User-Agent": myAgent,
+               "Cookie": myCookie + str(int(round(time.time()))) + "; 8017a_lastvisit=0	" + str(int(round(time.time()))) + "	/index.php"}
+            response13 = requests.post(onlineTimeToHuoyueUrl, headers=headers, data=formData2)
+            if response13.status_code == 200:
+                if ('完成积分转换') in response13.text:
+                    print('完成积分转换')
+                    huoyueAddedByScript = huoyueAddedByScript + int(transOnTime)
+                    print('现在活跃度：' + str(huoyueAddedByScript))
+        else:
+            print('时辰不足，无法签到')
+        time.sleep(3)
+        
+# 回复一帖
+if ifReply:
+    replyUrl = 'https://moeshare.cc/post.php?fid=16&nowtime=' + str(int(round(time.time()))) + '&verify=' + myveri
     formData3 = {
-        "integralsubmit": '1',
-        "Submit": '保存积分'
+        'atc_usesign': '1',
+        'replytouser': '',
+        'atc_convert': '1',
+        'atc_autourl': '1',
+        'step': '2',
+        'type': 'ajax_addfloor',
+        'action': 'reply',
+        'fid': '16',
+        'cyid': '',
+        'tid': tid,
+        'stylepath': 'wind8black',
+        'ajax': '1',
+        'verify': myveri,
+        '_hexie': 'd8507211',
+        'iscontinue': '0',
+        'atc_title': 'Re:日常打卡，随机给回帖的人送活跃度',
+        'atc_content': '水一贴，每天打卡活跃一下。' + time.strftime("%Y%m%d", time.localtime()),
+        'attachment_1': '(二进制)',
+        'atc_desc1': ''
     }
     headers = {"User-Agent": myAgent,
-           "Cookie": myCookie + str(int(round(time.time()))) + "; 8017a_lastvisit=0	" + str(int(round(time.time()))) + "	/index.php"}
-    response12 = requests.post(saveOnlineTimeUrl, headers=headers, data=formData3)
-    if response12.status_code == 200:
-        if ('可以保存的积分小于1') in response12.text:
-            print('可以保存的现在时间小于1')
-        elif ('一共转存了'):
-            print('转存成功')
+       "Cookie": myCookie + str(int(round(time.time()))) + "; 8017a_lastvisit=0	" + str(int(round(time.time()))) + "	/index.php"}
+    response14 = requests.post(replyUrl, headers=headers, data=formData3)
+    if response14.status_code == 200:
+        print(response14.text)
+        if ('水一贴，每天打卡活跃一下。') in response14.text:
+            print('回帖成功')
+            huoyueAddedByScript = huoyueAddedByScript + 1
+            print('现在活跃度：' + str(huoyueAddedByScript))
     time.sleep(3)
-    print(10 - huoyueAddedByScript)
-    print(int(onlineTime) / 2 + 1)
-    print((math.ceil((10 - huoyueAddedByScript) / 2)))
-    if 10 - huoyueAddedByScript < (int(onlineTime) / 2 + 1):
-        onlineTimeToHuoyueUrl = 'https://moeshare.cc/userpay.php?action=change&'
-        transOnTime = str(math.ceil((10 - huoyueAddedByScript) / 2))
-        formData2 = {
-            "step": '3',
-            "type": '3_currency',
-            "verify": myveri,
-            "change": transOnTime
-        }
-        print(transOnTime + '在线时间转换成活跃度')
-        headers = {"User-Agent": myAgent,
-           "Cookie": myCookie + str(int(round(time.time()))) + "; 8017a_lastvisit=0	" + str(int(round(time.time()))) + "	/index.php"}
-        response13 = requests.post(onlineTimeToHuoyueUrl, headers=headers, data=formData2)
-        if response13.status_code == 200:
-            if ('完成积分转换') in response13.text:
-                print(response13.text)
-                huoyueAddedByScript = huoyueAddedByScript + int(transOnTime)
-    time.sleep(3)
-
+    
 # def writefile(filereadlines):
     # # 写入日志文件
     # newfile = open('/srv/script/meng_qiandao.json', mode='w', encoding='UTF-8')
@@ -237,4 +283,5 @@ if (huoyueAddedByScript >= 10):
             time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + '\n')
             #outputData = {'meng_qiandao': time.strftime("%Y-%m-%d", time.localtime())}
             #writefile(json.dumps(outputData))
+            
 input()
