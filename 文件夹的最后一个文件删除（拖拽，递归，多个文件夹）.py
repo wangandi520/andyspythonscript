89e0c6d432704030d423293aa7daaa1ea8c02012
wangandi520 <wangandi520@gmail.com>
2022-11-05 14:52:49 +0800

    支持子文件夹

--- 文件夹的最后一个文件删除（拖拽，递归，多个文件夹）.py
+++ 文件夹的最后一个文件删除（拖拽，递归，多个文件夹）.py
@@ -3,22 +3,34 @@
 
 from pathlib import Path
 import sys
-    
+
+def getLastFile(filePath):
+    # type(filePath): Path
+    allFilePath = []
+    for file in Path(filePath).glob('*'):
+        if Path.is_file(file):
+            allFilePath.append(file)
+    allFilePath.sort()
+    return allFilePath[-1]
+
 def main(inputPath):
-    allallFilePath = []
+    # type(inputPath): Path
+    allFilePath = []
+    allDir = []
+    # 读取所有被拖拽的文件夹和子文件夹
     for aPath in inputPath[1:]:
         if Path.is_dir(Path(aPath)):
-            allFilePath = []
-            for file in Path(aPath).glob('**/*'):
-                if Path.is_file(file):
-                    allFilePath.append(file)
-            allFilePath.sort()     
-            allallFilePath.append(allFilePath[-1])
-    for eachFile in allallFilePath:
+            allDir.append(Path(aPath))
+        for file in Path(aPath).glob('**/*'):
+            if Path.is_dir(file):
+                allDir.append(file)
+    for eachDir in allDir:
+        allFilePath.append(getLastFile(eachDir))
+    for eachFile in allFilePath:
         print(eachFile)
-    myChoice = input('是否要删除以上文件, (y)es or (n)o:')
+    myChoice = input('要删除以上' + str(len(allFilePath)) + '个文件, 请输入y或yes：')
     if myChoice.lower() in ['y', 'yes']:
-        for eachFile in allallFilePath:
+        for eachFile in allFilePath:
             eachFile.unlink()
                 
 if __name__ == '__main__':
