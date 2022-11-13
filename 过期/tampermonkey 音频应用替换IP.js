0575996f6a8fed5ff8b66d76e17c15cc95a238b5
wangandi520 <wangandi520@gmail.com>
2022-11-13 12:31:29 +0800

    Create tampermonkey 音频应用替换IP.js

---
+++ 过期/tampermonkey 音频应用替换IP.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name 音频应用替换IP
+// @namespace http://tampermonkey.net/
+// @version 0.1
+// @Description try to take over the world!
+// @Author xup
+// @match http://43.225.39.42/
+// @grant none
+// ==/UserScript==
+
+var getLinks = document.getElementsByTagName('a');
+for(var i=0; i< getLinks.length; i++){
+    if(getLinks[i].href.indexOf("http://www.audiobar.net.cn")!==-1) {
+    getLinks[i].href = decodeURIComponent(getLinks[i].href.replace('http://www.audiobar.net.cn', 'http://43.225.39.42'));
+    }
+}
