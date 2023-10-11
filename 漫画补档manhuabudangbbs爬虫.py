90b1fd98d1f33fb062b6bd87acae5e1ee3da0133
wangandi520 <wangandi520@gmail.com>
2023-10-11 10:13:32 +0800

    Update 漫画补档manhuabudangbbs爬虫.py

--- 漫画补档manhuabudangbbs爬虫.py
+++ 漫画补档manhuabudangbbs爬虫.py
@@ -20,21 +20,22 @@ def writefile(fileName, filereadlines):
         newfile.writelines(filereadlines)
 
 def main():
-    for eachtid in range(1, 1974):
+    for eachtid in range(1, 500):
         url = 'https://www.manhuabudangbbs.com/read-htm-tid-' + str(eachtid) + '.html'
         getHtml = requests.get(url)
         try:
             soup = BeautifulSoup(getHtml.text, 'html.parser')
             getName = soup.select('#subject_tpc')[0].get_text()[:-7]
             print(url + ' ' + getName)
-            getTime = '\n\n' + soup.select('#td_tpc > div.tipTop.s6 > span:nth-child(3)')[0].get_text() + '\n'
+            getAuthor = soup.select('.readName.b a')[0].get_text() + ' '
+            getTime = soup.select('#td_tpc > div.tipTop.s6 > span:nth-child(3)')[0].get_text() + '\n'
             getContent = soup.select('#td_tpc > div.tpc_content')
             # 替换成换行
             myReplace = ['<br/>', '<br>', '</blockquote>', '</div>']
             for eachReplace in myReplace:
                 getContent = str(getContent).replace(eachReplace, '\n')
             getContent = BeautifulSoup(getContent, 'html.parser').get_text()
-            writefile(getName, getName + '\n' + url + getTime + getContent)
+            writefile(getName, getName + '\n\n' +  getAuthor +getTime + '\n\n' + getContent + '\n\n'  +url)
         except:
             print(str(eachtid) + '帖子错误')
         time.sleep(2)     
