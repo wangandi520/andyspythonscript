3f8f11ba364583ba2e9bfb356adee5baf97763df
wangandi520 <wangandi520@gmail.com>
2024-01-29 15:47:23 +0800

    两种模式

--- 根据rarzip文件是否包含文件夹分类（拖拽，多个文件或文件夹）.py
+++ 根据rarzip文件是否包含文件夹分类（拖拽，多个文件或文件夹）.py
@@ -6,95 +6,66 @@ import sys
 import zipfile
 import rarfile
 
+def doClassifyFile(inputPath):
+    # workMode = 1只显示是否包含文件夹不移动文件，workMode = 2会把分好类的文件移动到含文件夹，不含文件夹两个文件夹中
+    workMode = 1
+    withSubDir = []
+    noSubDir = []
+    file = inputPath
+    if file.suffix == '.rar' and rarfile.is_rarfile(file):
+        rf = rarfile.RarFile(file)
+        noDir = True
+        for eachFile in rf.infolist():
+            if eachFile.is_dir():
+                noDir = False
+                if file not in withSubDir:
+                    withSubDir.append(file)
+                break
+        if noDir and (file not in noSubDir):
+            noSubDir.append(file)
+        rf.close()
+    if file.suffix == '.zip' and zipfile.is_zipfile(file):
+        zf = zipfile.ZipFile(file)
+        noDir = True
+        for eachFile in zf.infolist():
+            if eachFile.is_dir():
+                noDir = False
+                if file not in withSubDir:
+                    withSubDir.append(file)
+                break
+        if noDir and (file not in noSubDir):
+            noSubDir.append(file)
+        zf.close()
+    withSubDirPath = Path(file).parent.joinpath(Path('含文件夹'))
+    noSubDirPath = Path(file).parent.joinpath(Path('不含文件夹'))
+    if workMode  == 1:
+        for file in withSubDir:
+            print('  含文件夹 ' + str(file))
+        for file in noSubDir:
+            print('不含文件夹 ' + str(file))
+    if workMode == 2:
+        if not withSubDirPath.exists() and withSubDir:
+            Path.mkdir(withSubDirPath)
+        if not noSubDirPath.exists() and noSubDir:
+            Path.mkdir(noSubDirPath)
+        for file in withSubDir:
+            print('  含文件夹 ' + str(file))
+            Path(file).replace(withSubDirPath.joinpath(file.name))
+        for file in noSubDir:
+            print('不含文件夹 ' + str(file))
+            Path(file).replace(noSubDirPath.joinpath(file.name))
+            
 def main(inputPath):
     del inputPath[0]
+    allFilePath = []
     for aPath in inputPath:
         if Path.is_dir(Path(aPath)):
-            withSubDir = []
-            noSubDir = []
-            for file in Path(aPath).glob('*.rar'):
-                if rarfile.is_rarfile(file):
-                    rf = rarfile.RarFile(file)
-                    noDir = True
-                    print(rf.namelist())
-                    for eachFile in rf.infolist():
-                        if eachFile.is_dir():
-                            noDir = False
-                            if file not in withSubDir:
-                                withSubDir.append(file)
-                            break
-                    if noDir and (file not in noSubDir):
-                        noSubDir.append(file)
-                    rf.close()
-                                        
-            for file in Path(aPath).glob('*.zip'):
-                if zipfile.is_zipfile(file):
-                    zf = zipfile.ZipFile(file)
-                    noDir = True
-                    for eachFile in zf.infolist():
-                        if eachFile.is_dir():
-                            noDir = False
-                            if file not in withSubDir:
-                                withSubDir.append(file)
-                            break
-                    if noDir and (file not in noSubDir):
-                        noSubDir.append(file)
-                    zf.close()
-            
-            withSubDirPath = Path(aPath).joinpath(Path('包含文件夹'))
-            noSubDirPath = Path(aPath).joinpath(Path('仅包含文件'))
-            if not withSubDirPath.exists() and withSubDir:
-                Path.mkdir(withSubDirPath)
-            if not noSubDirPath.exists() and noSubDir:
-                Path.mkdir(noSubDirPath)
-                
-            for file in withSubDir:
-                Path(file).replace(withSubDirPath.joinpath(file.name))
-            for file in noSubDir:
-                Path(file).replace(noSubDirPath.joinpath(file.name))
-        
+            for file in Path(aPath).glob('**/*'):
+                allFilePath.append(file)
         if Path.is_file(Path(aPath)):
-            withSubDir = []
-            noSubDir = []
-            file = Path(aPath)
-            if rarfile.is_rarfile(file):
-                rf = rarfile.RarFile(file)
-                noDir = True
-                print(rf.namelist())
-                for eachFile in rf.infolist():
-                    if eachFile.is_dir():
-                        noDir = False
-                        if file not in withSubDir:
-                            withSubDir.append(file)
-                        break
-                if noDir and (file not in noSubDir):
-                    noSubDir.append(file)
-                rf.close()
-                                        
-            if zipfile.is_zipfile(file):
-                zf = zipfile.ZipFile(file)
-                noDir = True
-                for eachFile in zf.infolist():
-                    if eachFile.is_dir():
-                        noDir = False
-                        if file not in withSubDir:
-                            withSubDir.append(file)
-                        break
-                if noDir and (file not in noSubDir):
-                    noSubDir.append(file)
-                zf.close()
-            
-            withSubDirPath = Path(aPath).parent.joinpath(Path('包含文件夹'))
-            noSubDirPath = Path(aPath).parent.joinpath(Path('仅包含文件'))
-            if not withSubDirPath.exists() and withSubDir:
-                Path.mkdir(withSubDirPath)
-            if not noSubDirPath.exists() and noSubDir:
-                Path.mkdir(noSubDirPath)
-                
-            if withSubDir:
-                Path(file).replace(withSubDirPath.joinpath(file.name))
-            if noSubDir:
-                Path(file).replace(noSubDirPath.joinpath(file.name))
+            allFilePath.append(Path(aPath))
+    for eachFilePath in allFilePath:
+        doClassifyFile(eachFilePath)
         
 if __name__ == '__main__':
     try:
