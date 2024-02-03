7950327af42ba7f86ad0e9db60e4de7cce20b096
wangandi520 <wangandi520@gmail.com>
2024-02-03 16:57:25 +0800

    Update 统计含数字的文件名可能的缺失情况（拖拽，递归，仅文件夹）.py

--- 统计含数字的文件名可能的缺失情况（拖拽，递归，仅文件夹）.py
+++ 统计含数字的文件名可能的缺失情况（拖拽，递归，仅文件夹）.py
@@ -7,14 +7,27 @@ import os
 
 def checkFileNameIndex(filePath):
     # 如果image001.jpg后是image003.jpg，则输出可能缺失的文件image002.jpg
-    # 设置文件名最后几位是数字，image001是3
+    # 设置文件名最后几位是序号，image001是3
     getIndexNumber = 3
+    # 第一个文件的序号
+    getFirstIndex = 0
     # 文件夹
     allFilePath = []
     getFlag = True
     if Path.is_dir(Path(filePath)):
         for eachFilePath in Path(filePath).glob('*'):
             allFilePath.append(eachFilePath)
+    # 推测文件名数字序号位数
+    getGuessIndex = 0
+    for tempIndex in range(-1, -6, -1):
+        if str(allFilePath[0].stem[tempIndex]).isdigit():
+            getGuessIndex = getGuessIndex + 1
+        else:
+            break
+    if getGuessIndex != getIndexNumber:
+        # 去掉下一行的#，直接使用程序推测的数字位数
+        getIndexNumber = getGuessIndex
+        #print('程序文件名序号位数应是' + str(getGuessIndex) + '，请修改第11行')
     # 符合这几个条件才继续执行脚本：文件夹不包含子文件夹，所有文件名长度相同，扩展名相同
     firstFileLength = len(allFilePath[0].stem)
     firstFileSuffix = allFilePath[0].suffix
@@ -27,15 +40,21 @@ def checkFileNameIndex(filePath):
             getFlag = False
     if getFlag:
         # 只显示可能缺失的文件名
-        for fileIndex in range(1, len(allFilePath)):
-            previousFileIndex = allFilePath[fileIndex - 1].name[firstFileLength - getIndexNumber : firstFileLength]
-            currentFileIndex = allFilePath[fileIndex].name[firstFileLength - getIndexNumber : firstFileLength]
-            if previousFileIndex.isdigit() and currentFileIndex.isdigit():
-                if int(currentFileIndex) - int(previousFileIndex) != 1:
-                    # 输出：可能缺失的文件名  文件夹名
-                    print(allFilePath[fileIndex - 1].name[0 : firstFileLength - getIndexNumber] + str(int(previousFileIndex) + 1).zfill(getIndexNumber) + firstFileSuffix + '  ' + Path(allFilePath[0]).parent.name)
-            else:
-                print('其他错误：文件名数字位数错误或其他')
+        firstFileStem = allFilePath[0].name[0 : firstFileLength - getIndexNumber]
+        # 这个文件夹的文件名序号
+        myFileNameIndex = []
+        for eachIndex in allFilePath:
+           myFileNameIndex.append(eachIndex.name[firstFileLength - getIndexNumber : firstFileLength])
+        # 不缺文件的情况下的文件序号
+        allFileNameIndex = []
+        for tempIndex in range(getFirstIndex, int(allFilePath[-1].name[firstFileLength - getIndexNumber : firstFileLength]) + 1):
+            allFileNameIndex.append(str(tempIndex).zfill(getIndexNumber))
+        for tempIndex in allFileNameIndex:
+            if tempIndex not in myFileNameIndex:
+                # 输出格式：可能缺失的文件名  文件夹名
+                #print(firstFileStem + tempIndex + firstFileSuffix + '  ' + str(filePath.name))
+                # 输出格式：可能缺失的文件名
+                print(firstFileStem + tempIndex + firstFileSuffix)
 
 def main(inputPath):
     del inputPath[0]
