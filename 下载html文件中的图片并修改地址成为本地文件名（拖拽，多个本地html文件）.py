25390194c09c7987609db2170897918b4b7049b0
wangandi520 <wangandi520@gmail.com>
2024-02-23 10:56:25 +0800

    Update 下载html文件中的图片并修改地址成为本地文件名（拖拽，多个本地html文件）.py

--- 下载html文件中的图片并修改地址成为本地文件名（拖拽，多个本地html文件）.py
+++ 下载html文件中的图片并修改地址成为本地文件名（拖拽，多个本地html文件）.py
@@ -1,7 +1,7 @@
 # encoding:utf-8
 # https://github.com/wangandi520/ClippingsToMarkdown
 # Programmed by Andy
-# v0.1
+# v0.2
 
 from pathlib import Path
 from bs4 import BeautifulSoup
@@ -9,41 +9,57 @@ import sys
 import time
 import requests
 
-def main(inputPath):
-    # 获取本地html文件内所有img的src地址
-    # 第一个文件的编号
-    fileIndex = 1
-    # 文件名前缀
-    fileNamePrefix = 'image'
-    # 文件名序号位数，3 = 000-999
-    fileNameFill = 3
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.55 Safari/537.36'}
-    del inputPath[0]
-    for aPath in inputPath:
-        if Path.is_file(Path(aPath)):
-            if (Path(aPath).suffix == '.html'):
-                print('正在处理的文件 ' + Path(aPath).name)
-                with open(Path(aPath), 'r', encoding = 'utf-8') as file:
-                    myHtml = file.read()
-                soup = BeautifulSoup(myHtml, 'html.parser')
-                allImages = soup.find_all('img')
-                for eachImage in allImages:
-                    src = eachImage.get('src')
-                    # 修改html文件中图片的src地址
-                    eachImage['src'] = '../Images/' + fileNamePrefix + str(fileIndex).zfill(fileNameFill) + '.jpg'
-                    imgData = requests.get(url=src, headers=headers).content
-                    imgPath = fileNamePrefix + str(fileIndex).zfill(fileNameFill) + '.jpg'
-                    # 保存图片
-                    with open(imgPath, 'wb') as f:
-                        f.write(imgData)
-                    # 保存html
-                    with open(Path(aPath), 'w', encoding = 'utf-8') as file:
-                        file.write(str(soup))
-                    fileIndex = fileIndex + 1
-                # 改名
+def readfile(filename):
+    with open(filename, mode='r', encoding='UTF-8') as file:
+        filereadlines = file.readlines()
+    return filereadlines
 
-   
-        
+def writefile(filename,filereadlines):
+    newfile = open(filename, mode='w', encoding='UTF-8')
+    newfile.writelines(filereadlines)
+    newfile.close()
+    
+def main(inputPath):
+    # html会被修改
+    # 获取本地html文件内所有img的src地址
+    for aPath in inputPath[1:]:
+        if Path.is_file(Path(aPath)) and Path(aPath).suffix == '.html':
+            # 第一个文件的编号
+            fileIndex = 1
+            # 文件名序号位数，3 = 000-999
+            fileNameFill = 3
+            # 文件名前缀
+            #fileNamePrefix = 'image'
+            fileNamePrefix = Path(aPath).stem
+            print('正在处理的文件 ' + Path(aPath).name)
+            oldHtmlFile = readfile(Path(aPath))
+            newHtmlFile = []
+            # 解析图片src
+            with open(Path(aPath), 'r', encoding = 'utf-8') as myFile:
+                mySoupHtml = myFile.read()
+            soup = BeautifulSoup(mySoupHtml, 'html.parser')
+            allImages = soup.find_all('img')
+            tempImageName = []
+            for tempIndex in range(0, len(allImages)):
+                getFileName = str(Path(allImages[tempIndex].get('src')).name)
+                getPartUrl = allImages[tempIndex].get('src').replace(getFileName, '')
+                if getPartUrl[0:4] == 'http':
+                    # [不含图片名的url，原图片名，保存的图片名]
+                    tempImageName.append([getPartUrl, getFileName, fileNamePrefix + str(tempIndex + 1).zfill(fileNameFill) + Path(allImages[tempIndex].get('src')).suffix])
+            print('需要下载' + str(len(allImages)) + '张图片')
+            for tempIndex in range(0, len(oldHtmlFile)):
+                for eachImage in tempImageName:
+                    if (eachImage[0] + eachImage[1]) in oldHtmlFile[tempIndex]:
+                        oldHtmlFile[tempIndex] = oldHtmlFile[tempIndex].replace((eachImage[0] + eachImage[1]), eachImage[2])
+            for eachImage in tempImageName:
+                imgData = requests.get(url=eachImage[0] + eachImage[1]).content
+                print('正在下载 ' + eachImage[0] + eachImage[1])
+                # 保存图片
+                with open(eachImage[2], 'wb') as imageFile:
+                    imageFile.write(imgData)
+            # 写入原html文件
+            writefile(Path(aPath).name, oldHtmlFile)
+            
 if __name__ == '__main__':
     try:
         if len(sys.argv) >= 2:
