0b3f5020483321f99ec4f4d57d8ddee7ce4c3c5a
wangandi520 <wangandi520@gmail.com>
2024-02-28 11:18:08 +0800

    Create 模板.py

---
+++ 模板.py
@@ -0,0 +1,42 @@
+# encoding:utf-8
+# https://github.com/wangandi520/andyspythonscript
+# by Andy
+# v0.1
+
+from pathlib import Path
+import sys
+
+def writefile(fileName, allFileContent):
+    with open(fileName, mode='w', encoding='UTF-8') as newfile:
+        newfile.writelines(allFileContent)
+
+def readfile(fileName):
+    with open(fileName, mode='r', encoding='UTF-8') as newfile:
+        allFileContent = newfile.readlines()
+    return allFileContent
+
+def doConvert(fileName):
+    # readFileContent = readfile(filename)
+    # for eachLine in readFileContent:
+        # print(eachLine)
+    # newFileName = filename.parent.joinpath(filename.stem + '.html')
+    # if not Path(newFileName).exists():
+        # writefile(newFileName, ttempFileContent)
+        
+def main(inputPath):
+    fileType = ['.txt']
+    for aPath in inputPath[1:]:
+        if Path.is_dir(Path(aPath)):
+            for eachFile in Path(aPath).glob('**/*'):
+                if (Path(eachFile).suffix in fileType):
+                    doConvert(Path(eachFile))
+        if Path.is_file(Path(aPath)):
+            if (Path(aPath).suffix in fileType):
+                doConvert(Path(aPath))
+
+if __name__ == '__main__':
+    try:
+        if len(sys.argv) >= 2:
+            main(sys.argv)
+    except IndexError:
+        pass
