583240e4e8f531f9e40fbfbcef6331b5576e25a4
wangandi520 <wangandi520@gmail.com>
2024-03-05 20:47:47 +0800

    Update 复制出epub的封面图片（拖拽，递归，多个文件或文件夹）.py

--- 复制出epub的封面图片（拖拽，递归，多个文件或文件夹）.py
+++ 复制出epub的封面图片（拖拽，递归，多个文件或文件夹）.py
@@ -14,16 +14,27 @@ def getEpubCoverImage(filePath):
     fileType = ['.epub']
     # 封面图片名
     coverImageFilename = ['cover.jpg', 'cover.jpeg']
+    # 图片复制到哪个位置
+    #copyToLocation = 'epub'，epub目录下
+    #copyToLocation = 'script'，脚本目录下
+    copyToLocation = 'epub'
     if Path.is_file(filePath) and (filePath.suffix.lower() in fileType):
         print('正在处理epub：' + filePath.name)
+        coverImageExist = False
         with zipfile.ZipFile(filePath, 'r') as myzipfile:
             # 获取所有文件列表
             eachFileList = myzipfile.namelist()
             for eachFilePath in eachFileList:
                 if Path(eachFilePath).name.lower() in coverImageFilename:
                     imageData = myzipfile.read(eachFilePath)
-                    imageFilePath = filePath.parent.joinpath(Path(filePath).stem + Path(eachFilePath).suffix)
+                    if copyToLocation == 'epub':
+                        imageFilePath = filePath.parent.joinpath(Path(filePath).stem + Path(eachFilePath).suffix)
+                    if copyToLocation == 'script':
+                        imageFilePath = Path(sys.argv[0]).parent.joinpath(Path(filePath).stem + Path(eachFilePath).suffix)
                     Path(imageFilePath).write_bytes(imageData)
+                    coverImageExist = True
+        if not coverImageExist:
+            print('未找到封面文件：' + filePath.name)
 
 def main(inputPath):
     for aPath in inputPath[1:]:
