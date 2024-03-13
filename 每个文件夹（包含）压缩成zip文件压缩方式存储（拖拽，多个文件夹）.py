d87758e1092667e989a8a5a16c7aff79b967a364
wangandi520 <wangandi520@gmail.com>
2024-03-13 22:15:28 +0800

    Update 每个文件夹（包含）压缩成zip文件压缩方式存储（拖拽，多个文件夹）.py

--- 每个文件夹（包含）压缩成zip文件压缩方式存储（拖拽，多个文件夹）.py
+++ 每个文件夹（包含）压缩成zip文件压缩方式存储（拖拽，多个文件夹）.py
@@ -1,20 +1,27 @@
 # encoding:utf-8
 # https://github.com/wangandi520/andyspythonscript
 # by Andy
-# v0.1
+# v0.2
 
 from pathlib import Path
 import sys
 import zipfile
 
 def doZipFolder(inputPath):
-    # 每个文件夹压缩成zip文件，包含这个文件夹，zip文件=文件夹名，压缩方式：存储（速度最快）
+    # 每个文件夹压缩成zip文件，包含这个文件夹，zip文件=文件夹名
     allFilePath = []
+    # 压缩方式：存储（速度最快），体积和压缩前差不多
+    myZipType = zipfile.ZIP_STORED
+    # 压缩方式：标准（速度一般），体积会变小
+    #myZipType = zipfile.ZIP_DEFLATED
+    # 保存位置：在脚本所在的目录
+    #myZipSavePath = inputPath.stem + '.zip'
+    # 保存位置：在目标文件夹所在的目录
+    myZipSavePath = inputPath.parent.joinpath(Path(inputPath.stem + '.zip'))
+    myZipFile = zipfile.ZipFile(myZipSavePath, 'w', myZipType)
+    print('正在新建压缩文件：' + str(myZipSavePath))
     for eachFilePath in inputPath.glob('**/*'):
-        allFilePath.append(eachFilePath.relative_to(inputPath.parent))
-    myZipFile = zipfile.ZipFile(inputPath.stem + '.zip', 'w', zipfile.ZIP_STORED)
-    for eachFilePath in allFilePath:
-        myZipFile.write(eachFilePath)
+        myZipFile.write(eachFilePath, eachFilePath.relative_to(inputPath.parent))
     myZipFile.close()
 
 def main(inputPath):
