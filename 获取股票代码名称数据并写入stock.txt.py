22397fdb55c186f11cd01f73951cef3e2740b2e3
wangandi520 <wangandi520@gmail.com>
2024-10-09 19:00:56 +0800

    Update 获取股票代码名称数据并写入stock.txt.py

--- 获取股票代码名称数据并写入stock.txt.py
+++ 获取股票代码名称数据并写入stock.txt.py
@@ -6,7 +6,10 @@ from pypinyin import pinyin, lazy_pinyin, Style
 writeData = []
 res_df = adata.stock.info.all_code()
 for tempIndex in range(0, len(res_df['stock_code'])):
-    writeData.append(res_df['exchange'][tempIndex].lower() + res_df['stock_code'][tempIndex] + ',' + ''.join(lazy_pinyin(res_df['short_name'][tempIndex], style=Style.FIRST_LETTER)) + ',' + res_df['short_name'][tempIndex] + '\n')
+    # 格式1分成每行
+    # writeData.append(res_df['exchange'][tempIndex].lower() + res_df['stock_code'][tempIndex] + ',' + ''.join(lazy_pinyin(res_df['short_name'][tempIndex], style=Style.FIRST_LETTER)) + ',' + res_df['short_name'][tempIndex] + '\n')
+    # 格式2写成一个列表
+    writeData.append("'" + res_df['exchange'][tempIndex].lower() + res_df['stock_code'][tempIndex] + ',' + ''.join(lazy_pinyin(res_df['short_name'][tempIndex], style=Style.FIRST_LETTER)) + ',' + res_df['short_name'][tempIndex] + "',")
 newFile = open('stock.txt', mode='w', encoding='UTF-8')
 newFile.writelines(writeData)
 newFile.close()
