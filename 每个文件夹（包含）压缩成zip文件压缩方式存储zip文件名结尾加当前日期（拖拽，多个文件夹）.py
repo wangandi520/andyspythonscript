70fc7ddd05923eb890146451be52ec02ea62ca18
wangandi520 <wangandi520@gmail.com>
2025-04-03 23:23:04 +0800

    Create 每个文件夹（包含）压缩成zip文件压缩方式存储zip文件名结尾加当前日期（拖拽，多个文件夹）.py

---
+++ 每个文件夹（包含）压缩成zip文件压缩方式存储zip文件名结尾加当前日期（拖拽，多个文件夹）.py
@@ -0,0 +1,39 @@
+# encoding:utf-8
+# https://github.com/wangandi520/andyspythonscript
+# by Andy
+# v0.3
+
+from pathlib import Path
+import sys
+import zipfile
+import datetime
+
+def doZipFolder(inputPath):
+    # 每个文件夹压缩成zip文件，包含这个文件夹，zip文件=文件夹名
+    # 压缩方式：存储（速度最快），体积和压缩前差不多
+    myZipType = zipfile.ZIP_STORED
+    # 压缩方式：标准（速度一般），体积会变小
+    #myZipType = zipfile.ZIP_DEFLATED
+    # 保存位置：在脚本所在的目录
+    #myZipSavePath = inputPath.stem + '.zip'
+    # 保存位置：在目标文件夹所在的目录
+    # getTime，%Y%m%d%H%M%S分别是年月日时分秒，可以替换成自己需要的格式
+    getTime = datetime.datetime.now().strftime("%Y%m%d")
+    myZipSavePath = inputPath.parent.joinpath(Path(inputPath.stem + getTime + '.zip'))
+    myZipFile = zipfile.ZipFile(myZipSavePath, 'w', myZipType)
+    print('正在新建压缩文件：' + str(myZipSavePath))
+    for eachFilePath in inputPath.glob('**/*'):
+        myZipFile.write(eachFilePath, eachFilePath.relative_to(inputPath.parent))
+    myZipFile.close()
+
+def main(inputPath):
+    for aPath in inputPath[1:]:
+        if Path.is_dir(Path(aPath)):
+            doZipFolder(Path(aPath))
+
+if __name__ == '__main__':
+    try:
+        if len(sys.argv) >= 2:
+            main(sys.argv)
+    except IndexError:
+        pass
