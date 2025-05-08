c271a87b2edc2649167953ae355f9fb133fc3ec7
wangandi520 <wangandi520@gmail.com>
2025-05-08 09:05:13 +0800

    Update tampermonkey 网页自动跳转.js

--- tampermonkey 网页自动跳转.js
+++ tampermonkey 网页自动跳转.js
@@ -2,10 +2,10 @@
 // @name         网页自动跳转
 // @namespace    http://tampermonkey.net/
 // @version      2025-05-08
-// @description  try to take over the world!
-// @author       You
+// @description  网页自动跳转
+// @author       wangandi520
 // @match        https://jump2.bdimg.com/safecheck*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=bdimg.com
+// @match        https://c.pc.qq.com/*
 // @grant        none
 // ==/UserScript==
 
@@ -13,4 +13,10 @@
 if (document.querySelector('.btn.btn-next')) {
     const hrefValue = document.querySelector('.btn.btn-next').getAttribute('href');
     window.location.href = hrefValue;
+}
+
+// qq群
+const urlObj = new URL(window.location.href);
+if (urlObj.searchParams.get('url')) {
+    window.location.href = urlObj.searchParams.get('url');
 }
